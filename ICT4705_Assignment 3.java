4b1434b8aeea9d30e5fe1094a4bb7a0dc38e37c0
jbwork0323 <jbwork323@gmail.com>
2026-09-26 15:02:57 -0600

    update date on caller script

--- ICT4705_Assignment 3.java
+++ ICT4705_Assignment 3.java
@@ -1,7 +1,7 @@
 // create a new mydate object from the class in the file MyDate.Java
 class ICT4705_Assignment3 {
     public static void main(String[] args) {
-        MyDate date = new MyDate(26, 9, 2020);
+        MyDate date = new MyDate(26, 9, 2026);
         System.out.println("Date: " + date.getDay() + "/" + date.getMonth() + "/" + date.getYear());
     }
 }
